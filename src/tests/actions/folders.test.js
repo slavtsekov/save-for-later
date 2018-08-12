53ec6ba00f6e0082eb5500af93e9439f628eb91b
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-12 17:19:11 +0300

    Incorporate fixture in tests

--- src/tests/actions/folders.test.js
+++ src/tests/actions/folders.test.js
@@ -1,5 +1,6 @@
 import { addFolder, removeFolder, editFolder, setFolders } from '../../actions/folders';
 import folderActions from '../../actions/actionTypes';
+import folders from '../fixtures/folders';
 
 test('should setup add folder action object', () => {
     const folder = 'Books';
@@ -40,11 +41,6 @@ test('should setup edit folder action object', () => {
 });
 
 test('should setup set folders action object', () => {
-    const folders = [
-        { id: '1', folder: 'Folder 1' },
-        { id: '2', folder: 'Folder 2' },
-        { id: '3', folder: 'Folder 3' }
-    ];
     const action = setFolders(folders);
 
     const expected = {
