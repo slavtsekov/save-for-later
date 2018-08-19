5163e8f812b810361418cd1260f2170c7997121d
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-19 20:12:16 +0300

    Complete AddFolder tests

--- src/tests/components/AddFolder.test.js
+++ src/tests/components/AddFolder.test.js
@@ -6,3 +6,43 @@ test('should render AddFolder correctly', () => {
     const wrapper = shallow(<AddFolder />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should dispatch to store if folder value is provided', () => {
+    const value = 'Books';
+    const addFolderSpy = jest.fn();
+    const wrapper = shallow(<AddFolder addFolder={addFolderSpy} />);
+
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {},
+        target: {
+            elements: {
+                folder: {
+                    value
+                }
+            }
+        }
+    });
+
+    expect(addFolderSpy).toHaveBeenLastCalledWith(value);
+    expect(wrapper.state('error')).toBeUndefined();
+});
+
+test('should not dispatch to store if folder value is not provided', () => {
+    const value = '  ';
+    const addFolderSpy = jest.fn();
+    const wrapper = shallow(<AddFolder addFolder={addFolderSpy} />);
+
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {},
+        target: {
+            elements: {
+                folder: {
+                    value
+                }
+            }
+        }
+    });
+
+    expect(addFolderSpy).not.toHaveBeenCalled();
+    expect(wrapper.state('error')).toBe('Please provide folder name.');
+});
