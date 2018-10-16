86b96a47576080598364eeccad1bd1aefbf4404a
Slav Tsekov <slav.tsekov@gmail.com>
2018-10-16 22:52:40 +0300

    Retrieve added link data and redirect

--- src/components/AddLinkPage.js
+++ src/components/AddLinkPage.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import LinkForm from './LinkForm';
 
-const AddLinkPage = () => (
-    <div>
-        <h1>Add Link</h1>
-        <LinkForm />
-    </div>
-);
+class AddLinkPage extends React.Component {
+    onSubmit = (data) => {
+        console.log(data);
+        this.props.history.push('/');
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Add Link</h1>
+                <LinkForm onSubmit={this.onSubmit} />
+            </div>
+        );
+    }
+}
 
 export { AddLinkPage as default };
--- src/tests/components/AddLinkPage.test.js
+++ src/tests/components/AddLinkPage.test.js
@@ -6,3 +6,18 @@ test('should render AddLinkPage', () => {
     const wrapper = shallow(<AddLinkPage />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should redirect to home page on submit', () => {
+    const historyMock = { push: jest.fn() };
+    const wrapper = shallow(<AddLinkPage history={historyMock} />);
+    const data = {
+        title: 'Test',
+        url: 'http://test.te',
+        description: '',
+        folder: ''
+    };
+
+    wrapper.find('LinkForm').prop('onSubmit')(data);
+
+    expect(historyMock.push).toHaveBeenLastCalledWith('/');
+});
